8a19f60ce31736077e7285511e86434b93f012a0
jaymar85 <jermark851@gmail.com>
2022-11-09 17:01:17 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      hello nucamp!
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='directory/:campsiteId' element={<CampsiteDetailPage  />} />
+        <Route path='about' element={<AboutPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import { Container } from "reactstrap";
+import SubHeader from "../components/SubHeader";
+import DisplayList from "../features/display/DisplayList";
+
+const HomePage = () => {
+  return (
+    <Container>
+      <SubHeader current='Home' />
+      <DisplayList />
+    </Container>
+  );
+};
+
+export default HomePage;
